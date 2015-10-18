bab4cf245458f42ed4c728558a86b902c0ee2921
christopherdumas <christopherdumas@me.com>
2015-10-18 14:39:06 -0700

    Fixed date!

--- app/models/post.js
+++ app/models/post.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     title: DS.attr('string'),
-    timestamp: DS.attr('date'),
+    timestamp: DS.attr('string'),
     description: DS.attr('string'),
     body: DS.attr('string')
 });
